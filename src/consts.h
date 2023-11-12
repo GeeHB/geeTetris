a42404108e1dcafcf106ab442874fdc388aebc8d
Jérôme Henry-Barnaudière - GeeHB / JHB <j.henrybarnaudiere@gmail.com>
2023-11-12 09:42:06 +0100

    Début de la v0.3

--- src/consts.h
+++ src/consts.h
@@ -45,7 +45,7 @@ extern "C" {
 
 // App. infos
 #define APP_NAME     "geeTetris"
-#define APP_VERSION  "0.2 - beta 3"
+#define APP_VERSION  "0.3"
 #define APP_AUTHOR   "GeeHB"
 
 // Playfield's dimensions (in box units)
